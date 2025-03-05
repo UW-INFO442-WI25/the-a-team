8218c67cfa1f234e8307cb8bec310ddc71330d6d
ngo610 <ngo610@uw.edu>
2025-03-04 16:31:11 -0800

    Added text and styling to ResourcesPage

--- src/components/ResourcesPage.jsx
+++ src/components/ResourcesPage.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
 export function ResourcesPage() {
-    
+
 
     return (
         <>
-        <h1>Resources</h1>
+            <div className='resources-container'>
+                <div className='resources-content'>
+                    <h1>Resources</h1>
+                    <h2>UN SDG 11: Sustainable Cities and Communities</h2>
+                    <p>The United Nations’ eleventh goal, sustainable cities and communities, aims to make cities inclusive, safe, resilient, and sustainable. By 2030, they aim to ensure that everyone has access to sufficient, safe, and affordable housing and basic services.</p><br></br>
+
+                    {/* ADD MORE */}
+
+                    <h2>Affordable Housing Crisis</h2>
+                    <p>As of 2022, over half of the eight billion people in our world live in urban areas. By 2050, it is estimated that 70 percent of the world population will live in cities. More and more people are moving to cities faster than these cities can develop housing, public transit, services, and infrastructure. Developers cannot build new housing fast enough to meet demands, and the competition for an already limited supply of affordable housing options drives up housing prices.</p><br></br>
+
+                    <p>The affordable housing crisis affects many college students. Some students have to postpone their college plans or opt for either community college or a job instead of attending university. Others may either enroll part time, commute from home farther from campus, or work long hours in addition to their schooling <a href='https://www.aacu.org/liberaleducation/articles/the-campus-housing-crisis'>(Budd, 2024)</a>. Students of any income level are likely to struggle with the limited housing supply, and this especially affects low-income and marginalized students. Many students find it difficult to find housing options that fit their specific needs.</p><br></br>
+
+                    <h2>Resources for Students</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam laudantium error beatae, totam excepturi dolorum explicabo voluptates velit nulla temporibus? Nostrum odit quibusdam aliquam quos inventore molestiae tempora accusamus. Voluptates!</p><br></br>
+
+                    <h2>Moving Services on Campus</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae consequatur at placeat minima, hic recusandae quos natus, eveniet dolor asperiores error, voluptatum quam voluptatibus ipsam corporis suscipit assumenda iusto. Voluptate!</p><br></br>
+
+                    <h2>Video</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati quam ratione, consequuntur, optio libero blanditiis numquam molestias illo quia fugit cumque, eligendi nemo architecto atque sint! Nihil iusto veritatis laboriosam?</p>
+                </div>
+            </div>
         </>
     );
 }
